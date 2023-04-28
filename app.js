a67a217435b70faefc8a3352ffce923512e31e7e
YumaAoki123 <131954321+YumaAoki123@users.noreply.github.com>
2023-04-28 15:42:21 +0900

    Revert "todoアプリの完了未完了機能を実装しました。"

--- app.js
+++ app.js
@@ -19,10 +19,3 @@ function addTodo() {
         $("input").val("");
     }
 }
-$(document).on("change","input[type=checkbox]", function(){
-    if($(this).is(":checked")){
-        $(this).parent().css("text-decoration","line-through");
-    } else {
-        $(this).parent().css("text-decoration","none");
-    }
-});
